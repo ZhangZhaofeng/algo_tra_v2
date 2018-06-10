33c4df29fdfde20c752b0284ef88675f1d9ef501
dong yili <dong@tk2-410-46209.vs.sakura.ne.jp>
2018-06-10 17:11:16 +0900

    change hilo param

--- technical_fx_bidirc.py
+++ technical_fx_bidirc.py
@@ -24,11 +24,11 @@ class HILO:
         return x.T
 
     def get_HIGH_MA(self, HIGH):  # price=1*N (N>61)
-        ma_high=self.MA(HIGH,5)
+        ma_high=self.MA(HIGH,35)
         return ma_high
 
     def get_LOW_MA(self, LOW):  # price=1*N (N>61)
-        ma_low=self.MA(LOW,10)
+        ma_low=self.MA(LOW,40)
         return ma_low
 
     def get_long_price(self, HIGH):
